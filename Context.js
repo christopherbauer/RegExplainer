8ead54c6d172f61fa10079c860701f121e8882e3
Christopher Bauer <chrisbauer.career@gmail.com>
2015-05-07 14:02:32 -0400

    Added Context tracking
    Added Context.js that holds the logic for tracking contexts.
    
    cleaned up the logic for displaying the explanation text (needs a better
    explanation because it's still a little confusing to me when I first
    look at it)

---
+++ Context.js
@@ -0,0 +1,33 @@
+var contexts = {
+	None : 'N',
+	LogicalGroup: 'L',
+	ExplicitSet: 'E',
+	ExplicitQuantifier: 'Q'
+}
+function Context() {
+	var contextHistory = [];
+	this.getContext = function() {
+		if(contextHistory.length == 0) {
+			return contexts.None;
+		}
+		return contextHistory[contextHistory.length-1];
+	}
+	this.addContext = function(context) {
+		contextHistory.push(context);
+	}
+	this.removeCurrentContext = function() {
+		if(contextHistory.length == 0) {
+			throw new Error("IndexOutOfRangeError");
+		}
+		contextHistory.splice(-1);
+	}
+	this.removeContext = function(expectedContext) {
+		if(contextHistory.length == 0) {
+			throw new Error("IndexOutOfRangeError");
+		}
+		if(contextHistory[contextHistory.length-1] != expectedContext) {
+			throw new Error("ContextNotExpectedException");
+		}
+		contextHistory.splice(-1,1);
+	}
+}
